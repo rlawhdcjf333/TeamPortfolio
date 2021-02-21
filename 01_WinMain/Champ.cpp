4fa1864fd526ec5dccace0facb37c885da32f8ac
leekwangsub <79181792+leekwangsub@users.noreply.github.com>
2021-02-22 08:32:13 +0900

    champ
    champ

--- 01_WinMain/Champ.cpp
+++ 01_WinMain/Champ.cpp
@@ -18,21 +18,29 @@ void Champ::Init()
 
 void Champ::Release()
 {
+
 }
 
 void Champ::Update()
 {
-
 	//Champ°¡ ÇÒÀÏ
-
-
-
-
-
 }
 
 void Champ::Render(HDC hdc)
 {
 	mImage->AlphaScaleFrameRender(hdc, mRect.left, mRect.top, mCurrentAnm->GetNowFrameX(), mCurrentAnm->GetNowFrameY(), 100,100, mAlpha);
 
+}
+
+void Champ::SetEnemy(string enemy1, string enemy2, string enemy3)
+{
+	mEnemy1 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, enemy1);
+	mEnemy2 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, enemy2);
+	mEnemy3 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, enemy3);
+}
+
+void Champ::SetFriend(string friend1, string friend2)
+{
+	mFriend1 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, friend1);
+	mFriend2 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, friend2);
 }
--- 01_WinMain/Champ.h
+++ 01_WinMain/Champ.h
@@ -3,9 +3,22 @@
 class Animation;
 class Champ : public GameObject
 {
+protected:
 	Image* mImage;
-
 	Animation* mCurrentAnm;
+
+	GameObject* mEnemy1;
+	GameObject* mEnemy2;
+	GameObject* mEnemy3;
+
+	GameObject* mFriend1;
+	GameObject* mFriend2;
+	
+	float mHP;
+	float mMP;
+	float mAtk;
+	float mDef;
+	
 
 	float mAlpha;
 	
@@ -18,4 +31,6 @@ public:
 	void Update()override;
 	void Render(HDC hdc)override;
 	
+	virtual void SetEnemy(string enemy1, string enemy2, string enemy3);
+	virtual void SetFriend(string friend1, string friend2);
 };
