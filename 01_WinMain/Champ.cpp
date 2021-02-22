db597ebebe387f4d8d2f70a55ec81af80c024cc7
leekwangsub <79181792+leekwangsub@users.noreply.github.com>
2021-02-22 17:44:27 +0900

    챔피언
    챔피언 좀 만듬

--- 01_WinMain/Champ.cpp
+++ 01_WinMain/Champ.cpp
@@ -2,6 +2,9 @@
 #include "Champ.h"
 #include "Animation.h"
 
+Champ::Champ(const string & name)
+	:GameObject(name) {}
+
 Champ::Champ(const string& name, float x, float y) : GameObject(name)
 {
 	mX = x, mY = y;
@@ -9,11 +12,11 @@ Champ::Champ(const string& name, float x, float y) : GameObject(name)
 
 void Champ::Init()
 {
-
 	mSizeX = mImage->GetFrameWidth();
 	mSizeY = mImage->GetFrameHeight();
 	RectMakeCenter(mX, mY, mSizeX, mSizeY);
 
+	
 }
 
 void Champ::Release()
@@ -23,12 +26,6 @@ void Champ::Release()
 void Champ::Update()
 {
 
-	//Champ°¡ ÇÒÀÏ
-
-
-
-
-
 }
 
 void Champ::Render(HDC hdc)
@@ -36,3 +33,21 @@ void Champ::Render(HDC hdc)
 	mImage->AlphaScaleFrameRender(hdc, mRect.left, mRect.top, mCurrentAnm->GetNowFrameX(), mCurrentAnm->GetNowFrameY(), 100,100, mAlpha);
 
 }
+
+//void Champ::GetStaff(string staff)
+//{
+//	//mStaff = ObjectManager::GetInstance()->FindObject(ObjectLayer::Staff, staff);
+//}
+//
+//void Champ::GetEnemy(string enemy1, string enemy2, string enemy3)
+//{
+//	//mEnemy1 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, enemy1);
+//	//mEnemy2 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, enemy2);
+//	//mEnemy3 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, enemy3);
+//}
+//
+//void Champ::GetFriendly(string friendly1, string friendly2)
+//{
+//	//mFriendly1 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, friendly1);
+//	//mFriendly2 = ObjectManager::GetInstance()->FindObject(ObjectLayer::Champ, friendly2);
+//}
