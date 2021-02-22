077f637b8fcad8e0f100609473450a5db191ca0f
tambang95 <52881527+tambang95@users.noreply.github.com>
2021-02-22 14:29:59 +0900

    director, staff 수정

--- 01_WinMain/Director.h
+++ 01_WinMain/Director.h
@@ -5,17 +5,20 @@
 class Staff;
 class Director : public GameObject
 {
+	vector<string> mNameList;
+
 	Image* mTeamImage;
 	wstring mFileName;
 
-	vector<Image*> mTeamList;
-	map<wstring, Staff*> mStaffList;
+	//vector<Image*> mTeamList;
+	//map<wstring, Staff*> mStaffList;
 public:
 	Director(const string& name, const wstring& fileName);
+
+	string RandomName();
 
 	void Init()override;
 	void Release()override;
 	void Update()override;
 	void Render(HDC hdc)override;
-	void LoadFromFile(const wstring& fileName);
 };
