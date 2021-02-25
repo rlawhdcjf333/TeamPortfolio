d7085f20753c7f56eb4986ff23e76b9954708db6
leekwangsub <79181792+leekwangsub@users.noreply.github.com>
2021-02-25 17:50:41 +0900

    내꼬야
    기쁨의 포자춤

--- 01_WinMain/BulletManager.cpp
+++ 01_WinMain/BulletManager.cpp
@@ -1,3 +1,29 @@
 #include "pch.h"
 #include "BulletManager.h"
 #include "Animation.h"
+
+void BulletManager::Init(float startX,float startY, float endX, float endY)
+{
+	mX = startX;
+	mY = startY;
+	mEndX = endX;
+	mEndY = endY;
+
+	mAngle = Math::GetAngle(mX, mY, mEndX, mEndY);
+	mSpeed = 500.f;
+}
+
+void BulletManager::Release()
+{
+}
+
+void BulletManager::Update()
+{
+	mX += cosf(mAngle)*(Time::GetInstance()->DeltaTime())*mSpeed;	//이동속도만큼 X,Y값에 값을 더한다.
+	mY -= sinf(mAngle)* (Time::GetInstance()->DeltaTime())* mSpeed;
+	mRect = RectMakeCenter(mX / 10, mY / 10, mEndX / 10, mEndY / 10);
+}
+
+void BulletManager::Render(HDC hdc)
+{
+}
--- 01_WinMain/BulletManager.h
+++ 01_WinMain/BulletManager.h
@@ -1,5 +1,21 @@
 #pragma once
+#include "Animation.h"
 class BulletManager
 {
+private:
+	float mX;
+	float mY;
+	float mEndX;
+	float mEndY;
+
+	float mAngle;
+	float mSpeed;
+
+	RECT mRect;
+
+	void Init(float startX, float startY, float endX, float endY);
+	void Release();
+	void Update();
+	void Render(HDC hdc);
 };
 
